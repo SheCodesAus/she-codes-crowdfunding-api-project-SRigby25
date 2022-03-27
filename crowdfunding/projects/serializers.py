a8fbea6d4903d60e01fdd683cadc839a291e976c
SRigby25 <sian.rigby@hotmail.com.au>
2022-03-27 21:25:41 +0800

    Catergory serializer updated

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 from .models import Project, Pledge
 
+
+
 class PledgeSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     amount = serializers.IntegerField()
     comment = serializers.CharField(max_length=200)
     anonymous = serializers.BooleanField()
-    supporter = serializers.CharField(max_length=200)
+    supporter = serializers.ReadOnlyField(source='supporter.id')
     project_id = serializers.IntegerField()
+    
 
     def create(self, validated_data):
         return Pledge.objects.create(**validated_data)
@@ -23,7 +26,12 @@ class ProjectSerializer(serializers.Serializer):
     date_created = serializers.DateTimeField()
     # owner = serializers.CharField(max_length=200)
     owner = serializers.ReadOnlyField(source='owner.id')
-
+    category = serializers.CharField(max_length=8, default='1 Drink')
+        
+    def validate_category(self, value):
+        if value not in ["1 Drink", "2 Drink", "3 Drink", "4 Drink", "5+ Drink"]:
+            raise serializers.ValidationError("Category is not available")
+        return value
 
     def create(self, validated_data):
         return Project.objects.create(**validated_data)
